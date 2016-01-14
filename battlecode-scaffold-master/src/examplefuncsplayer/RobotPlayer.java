f6667bb77ee41ef0b3f31095f6fd23f335736be4
Pau <psurrellr@gmail.com>
2016-01-14 19:35:01 +0100

    prova

--- battlecode-scaffold-master/src/examplefuncsplayer/RobotPlayer.java
+++ battlecode-scaffold-master/src/examplefuncsplayer/RobotPlayer.java
@@ -10,6 +10,7 @@ public class RobotPlayer {
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
      * If this method returns, the robot dies!
      **/
+	//HOLA
     @SuppressWarnings("unused")
     public static void run(RobotController rc) {
         // You can instantiate variables here.
