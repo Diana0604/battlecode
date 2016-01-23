18ee9e273d39729d00cc7f0d3d7a1e4ced9edd50
DIANA <diana.vallverdu@gmail.com>
2016-01-23 17:12:55 +0100

    els TTMs intenten colocarse on toca

--- battlecode-scaffold-master/src/team375/Turret.java
+++ battlecode-scaffold-master/src/team375/Turret.java
@@ -2,6 +2,7 @@ package team375;
 
 import battlecode.common.Clock;
 import battlecode.common.Direction;
+import battlecode.common.GameActionException;
 import battlecode.common.GameConstants;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotInfo;
@@ -26,19 +27,165 @@ public class Turret extends RobotPlayer {
 	private static boolean TTM;
 	private static MapLocation loc;
 	
+	//busca on esta l'archon que l'ha creat
+	public static void buscaRef()
+	{
+		try
+		{
+			for(Direction dir : directions)
+			{
+				if(!dir.isDiagonal()) continue;
+				if(!rc.canSense(loc.add(dir))) continue; 
+				//TODO en principi les hauria de sense totes, i de fet aquest es un cas especial que segurament no s'ha construit a la diagonal. Pensar que fer
+				RobotInfo ri = rc.senseRobotAtLocation(loc.add(dir));
+				if(ri.equals(null)) continue;
+				if(!ri.type.equals(RobotType.ARCHON)) continue;
+				if(!ri.team.equals(myTeam)) continue;
+				ref = dir;
+				break;
+			}
+			
+			if(ref.equals(null)) ref = Direction.NORTH; //TODO parche per neutrals
+			
+		} catch(GameActionException e)
+		{
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	//busca si s'ha de moure a dreta/esquerra
+	public static void tryLeft()
+	{
+		try 
+		{
+			Direction left = ref;
+			
+			for(int i = 0; i < 2 && !set; ++i)
+			{
+				left.rotateLeft();
+				if(!rc.canSense(loc.add(left))) continue; //TODO de nou, no hauria de passar mai
+				RobotInfo ri = rc.senseRobotAtLocation(loc.add(left));
+				if(!ri.type.equals(null) && ri.team.equals(myTeam) && ri.type.equals(RobotType.TURRET)) continue;
+				rotation = LEFT;
+				set = true;
+				if(rc.canMove(left))
+				{
+					hasMoved = true;
+					diagonal = false;
+					rc.move(left); //TODO afegir que provi si li'n falten o massa lio? (despres al set ho fara, puc fer si no que d'alguna manera li digui al 
+					//primer set que comenci a provar en right/left
+				}
+				break;
+			}
+		} catch(GameActionException e)
+		{
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	public static void tryRight()
+	{
+		try 
+		{
+			Direction right = ref;
+			
+			for(int i = 0; i < 2 && !set; ++i)
+			{
+				right.rotateRight();
+				if(!rc.canSense(loc.add(right))) continue; //TODO de nou, no hauria de passar mai
+				RobotInfo ri = rc.senseRobotAtLocation(loc.add(right));
+				if(!ri.type.equals(null) && ri.team.equals(myTeam) && ri.type.equals(RobotType.TURRET)) continue;
+				rotation = RIGHT;
+				set = true;
+				if(rc.canMove(right))
+				{
+					hasMoved = true;
+					diagonal = false;
+					rc.move(right); //TODO afegir que provi si li'n falten o massa lio? (despres al set ho fara, puc fer si no que d'alguna manera li digui al 
+					//primer set que comenci a provar en right/left
+				}
+				break;
+			}
+		} catch(GameActionException e)
+		{
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	//intenta sortir de la diagonal
+	public static void goLeft()
+	{
+		try
+		{
+			Direction dir = ref;
+			for(int i = 0; i < 2; ++i)
+			{
+				dir = dir.rotateLeft();
+				if(rc.canMove(dir))
+				{
+					encallat = 0;
+					diagonal = false;
+					hasMoved = true;
+					rc.move(dir);
+					break;
+				}
+			}
+			//aqui es podria pensar que potser un robot no es mou a un lloc perque en aquell moment hi ha rubble podria esperar i 
+			//anarhi despres.
+			//pero en realitat si no s'hi mou i es mou mes enlla, despres una altra turret ja hi arribara TODO comentar
+			
+			if(!hasMoved) ++encallat;
+			
+		} catch(Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+	}
+	public static void goRight()
+	{
+		try
+		{
+			Direction dir = ref;
+			for(int i = 0; i < 2; ++i)
+			{
+				dir = dir.rotateRight();
+				if(rc.canMove(dir))
+				{
+					encallat = 0;
+					diagonal = false;
+					hasMoved = true;
+					rc.move(dir);
+					break;
+				}
+			}
+			//aqui es podria pensar que potser un robot no es mou a un lloc perque en aquell moment hi ha rubble podria esperar i 
+			//anarhi despres.
+			//pero en realitat si no s'hi mou i es mou mes enlla, despres una altra turret ja hi arribara TODO comentar
+			
+			if(!hasMoved) ++encallat;
+			
+		} catch(Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+	}
+	
 	public static void playTurret() {
 		try {
             attackRange = rc.getType().attackRadiusSquared;
-            loc = rc.getLocation();
             set = false;
             encallat = 0;
             primer = true;
+            TTM = false;
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
 
         while (true) {
+        	loc = rc.getLocation();
             // This is a loop to prevent the run() method from returning. Because of the Clock.yield()
             // at the end of it, the loop will iterate once per game round.
             try {
@@ -54,63 +201,13 @@ public class Turret extends RobotPlayer {
 	            		{
 	            			primer = false;
 	            			diagonal = true;
-	            			for(Direction dir : directions)
-	            			{
-	            				if(!dir.isDiagonal()) continue;
-	            				if(!rc.canSense(loc.add(dir))) continue;
-	            				RobotInfo ri = rc.senseRobotAtLocation(loc.add(dir));
-	            				if(ri.equals(null)) continue;
-	            				if(!ri.team.equals(myTeam)) continue;
-	            				if(!ri.type.equals(RobotType.ARCHON)) continue;
-	            				if(ri.team.equals(myTeam) && ri.type.equals(RobotType.ARCHON))
-	            				{
-	            					ref = dir;
-	            					break;
-	            				}
-	            			}
+	            			buscaRef();
 	            		} //TODO en ccas que per algo no estigui a la diagonal, fer algo 
 	            		
 	            		if(diagonal)
 	            		{
-	            			Direction left = ref.rotateLeft();
-	            			
-	            			if(!set)
-	            			{
-	            				for(int i = 0; i < 2 && !set; ++i)
-	            				{
-			            			if(rc.canSense(loc.add(left)))
-			            			{
-			            				RobotInfo ri = rc.senseRobotAtLocation(loc.add(left));
-			            				if(ri.equals(null) || !ri.team.equals(myTeam) || !ri.type.equals(RobotType.TURRET)) 
-			            				{
-			            					rotation = LEFT;
-			            					set = true;
-			            					break;
-			            				}
-			            			}
-			            			left = left.rotateLeft();
-	            				}
-	            			}
-	            			
-	            			if(!set)
-	            			{
-	            				Direction right = ref.rotateRight();
-	            				
-	            				for(int i = 0; i < 2 && !set; ++i)
-	            				{
-		            				if(rc.canSense(loc.add(right))) 
-		            				{
-		            					RobotInfo ri = rc.senseRobotAtLocation(loc.add(right));
-		            					if(ri.equals(null) || !ri.team.equals(myTeam) || !ri.type.equals(RobotType.TURRET)) 
-		                				{
-		                					rotation = RIGHT;
-		                					set = true;
-		                					break;
-		                				}
-		            					right = right.rotateRight();
-		            				}
-	            				}
-	            			}
+	            			if(!set) tryLeft();
+	            			if(!set) tryRight();
 	            			
 	            			if(!set)
 	            			{
@@ -127,57 +224,24 @@ public class Turret extends RobotPlayer {
 	            			
 	            			if(set)
 	            			{
-	            				if(rotation == LEFT)
-	            				{
-	            					Direction dir = ref.rotateLeft();
-	            					for(int i = 0; i < 3 && !hasMoved; ++i)
-	            					{
-		            					if(rc.canMove(dir))
-		            					{
-		            						encallat = 0;
-		            						diagonal = false;
-		            						hasMoved = true;
-		            						rc.move(dir);
-		            						break;
-		            					} 
-		            					//aqui es podria pensar que potser un robot no es mou a un lloc perque en aquell moment hi ha rubble podria esperar i 
-		            					//anarhi despres.
-		            					//pero en realitat si no s'hi mou i es mou mes enlla, despres una altra turret ja hi arribara TODO comentar
-		            					dir = dir.rotateLeft();
-	            					}
-	            				}
+	            				if(rotation == LEFT) goLeft();
+	            				if(rotation == RIGHT) goRight();
 	            				
-	            				if(rotation == RIGHT)
+	            				if(encallat > MAXENCALLAT)
 	            				{
-	            					Direction dir = ref.rotateRight();
-	            					for(int i = 0; i < 3 && !hasMoved; ++i)
+	            					if(rc.canMove(ref.opposite()))
 	            					{
-		            					if(rc.canMove(dir))
-		            					{
-		            						encallat = 0;
-		            						diagonal = false;
-		            						hasMoved = true;
-		            						rc.move(dir);
-		            						break;
-		            					}
-		            					dir = dir.rotateRight();
+	            						encallat = 0;
+	            						set = false;
+	            						hasMoved = true;
+	            						rc.move(ref.opposite()); //si fa MOLT que estic alla no val la pena seguir-ho intentant
 	            					}
-	            				}
-	            				
-	            				if(!hasMoved) 
-	            				{ 
-	            					++encallat;
-		            				if(encallat > MAXENCALLAT) 
-		            				{
-		            					if(rc.canMove(ref.opposite())) 
-		            					{
-		            						rc.move(ref.opposite()); //en plan si fa MOLT que estic alla no val la pena seguir-ho intentant
-		            						encallat = 0;
-		                					set = false;
-		                					hasMoved = true;
-		            					}
-		            					else {}; //TODO aqui hauriem de fer algo, tot i que es una situacio chunga, ja pensare
-		            				}
+	            					
+	            					if(!hasMoved) //aixo voldria dir que la turret no s'ha construit quan tocava o algo aixi
+	            					{
+	            						TTM = false;
+	            						rc.unpack();
+	            					}
 	            				}
 	            			}
 	            		}
@@ -202,40 +266,32 @@ public class Turret extends RobotPlayer {
 	            					{
 	            						++turretsFound;
 	            					}
-	            					else
-	            					{
-	            						if(rc.canMove(ref))
-	            						{
-	            							hasMoved = true;
-	            							encallat = 0;
-	            							rc.move(ref);
-	            						}
-	            					}
 	            				}
 	            				else ++turretsFound;
 	            			}
 	            			
+	            			Direction dir = null;
+	            			
 	            			if(!hasMoved)
 	            			{
-	            				Direction dir;
 	            				if(rotation == RIGHT) dir = ref.rotateRight();
 	            				else dir = ref.rotateLeft();
-	            				
+	            				if(rc.canMove(dir))
+	            				{
+	            					hasMoved = true; 
+	            					encallat = 0;
+	            					rc.move(dir);
+	            				}
+	            			}
+	            			
+	            			if(!hasMoved)
+	            			{
 	            				if(rc.canSenseLocation(loc.add(dir)))
 	            				{
 	            					RobotInfo ri = rc.senseRobotAtLocation(loc.add(dir));
 	            					if(!ri.equals(null) && ri.team.equals(myTeam) && (ri.type.equals(RobotType.TURRET) || ri.type.equals(RobotType.SCOUT)))
 	            					{
 	            						++turretsFound;
-	            					}
-	            					else
-	            					{
-	            						if(rc.canMove(dir))
-	            						{
-	            							hasMoved = true;
-	            							encallat = 0;
-	            							rc.move(dir);
-	            						}
 	            					}
 	            				}
 	            				else ++turretsFound;
@@ -269,7 +325,10 @@ public class Turret extends RobotPlayer {
             	
 	            	else 
 	            	{
-	            		if(primer) rc.pack();
+	            		if(primer) 
+	            		{
+	            				rc.pack();
+	            		}
 	            		else 
 	            		{
 	            			if (rc.isWeaponReady()) {
