af7a5c87d18450c0c39a7efdb90ed0e20cf29400
Pau <psurrellr@gmail.com>
2016-01-24 19:46:42 +0100

    afegits metodes a la classe utils

--- battlecode-scaffold-master/src/team375/Utils.java
+++ battlecode-scaffold-master/src/team375/Utils.java
@@ -6,4 +6,37 @@ public class Utils {
 	public static int distInfinit(MapLocation a, MapLocation b){
 		return Math.max(Math.abs(a.x-b.x), Math.abs(a.y-b.y));
 	}
+	 
+	//Suma dues direccions si una es horitzontal i l'altra vertical. Si no es compleix aixo, retorna null
+	public static Direction addDirections(Direction a, Direction b){
+		if (a == null || b == null){
+			System.out.println("Error, intentes sumar direccions nuls");
+			return null;
+		}
+		if (a.isDiagonal() || b.isDiagonal()){
+			System.out.println("Error, intentes sumar direccions diagonals");
+			return null;
+		}
+		if (a == Direction.NORTH){
+			if (b == Direction.EAST) return Direction.NORTH_EAST;
+			if (b == Direction.WEST) return Direction.NORTH_WEST;
+			else return null;
+		}
+		if (a == Direction.SOUTH){
+			if (b == Direction.EAST) return Direction.SOUTH_EAST;
+			if (b == Direction.WEST) return Direction.SOUTH_WEST;
+			else return null;
+		}
+		if (a == Direction.EAST){
+			if (b == Direction.NORTH) return Direction.NORTH_EAST;
+			if (b == Direction.SOUTH) return Direction.SOUTH_EAST;
+			else return null;
+		}
+		if (a == Direction.WEST){
+			if (b == Direction.NORTH) return Direction.NORTH_WEST;
+			if (b == Direction.SOUTH) return Direction.SOUTH_WEST;
+			else return null;
+		}
+		return null;
+	}
 }
