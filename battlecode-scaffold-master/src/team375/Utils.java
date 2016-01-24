b4931c485100d70fd4193854a04b5be45877b514
Pau <psurrellr@gmail.com>
2016-01-24 18:13:06 +0100

    Afegit metode distancia_infinit (maxim de la dif de coordenades)

--- battlecode-scaffold-master/src/team375/Utils.java
+++ battlecode-scaffold-master/src/team375/Utils.java
@@ -1,5 +1,9 @@
 package team375;
 
+import battlecode.common.*;
+
 public class Utils {
-	
+	public static int distInfinit(MapLocation a, MapLocation b){
+		return Math.max(Math.abs(a.x-b.x), Math.abs(a.y-b.y));
+	}
 }
