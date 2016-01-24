061e70a01c2ca9dcf96e7ea2e001aba6e67a17e5
DIANA <diana.vallverdu@gmail.com>
2016-01-24 12:04:55 +0100

    diria que les turrets van bastant be

--- battlecode-scaffold-master/src/team375/Turret.java
+++ battlecode-scaffold-master/src/team375/Turret.java
@@ -20,6 +20,8 @@ public class Turret extends RobotPlayer {
 	private static boolean TTM;
 	private static MapLocation loc;
 	
+	private static int pack;
+	
 	//busca on esta l'archon que l'ha creat
 	public static void buscaRef()
 	{
@@ -199,6 +201,7 @@ public class Turret extends RobotPlayer {
             primer = true;
             TTM = false;
             ref = Direction.NORTH;
+            pack = 0;
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
@@ -209,9 +212,7 @@ public class Turret extends RobotPlayer {
             // This is a loop to prevent the run() method from returning. Because of the Clock.yield()
             // at the end of it, the loop will iterate once per game round.
             try {
-                // If this robot type can attack, check for enemies within range and attack one
-            	
-            	if(rc.isCoreReady() && TTM)
+                if(TTM)
             	{
             		if(rc.isCoreReady())
             		{
@@ -343,45 +344,47 @@ public class Turret extends RobotPlayer {
             		}
             	}
             	
-	            	else 
-	            	{
-	            		if(primer) 
-	            		{
-	            			TTM = true;
-	            				rc.pack();
-	            		}
-	            		else 
-	            		{
-	            			MapLocation objective = readSignals();
-	            			if(objective != null)
-	            			{
-	            				if(rc.isWeaponReady()) rc.attackLocation(objective);
-	            			}
-	            			else
-	            			{
-		            			if (rc.isWeaponReady()) {
-			                    RobotInfo[] enemiesWithinRange = rc.senseNearbyRobots(attackRange, enemyTeam);
-			                    RobotInfo[] zombiesWithinRange = rc.senseNearbyRobots(attackRange, Team.ZOMBIE);
-			                    if (enemiesWithinRange.length > 0) {
-			                        for (RobotInfo enemy : enemiesWithinRange) {
-			                            // Check whether the enemy is in a valid attack range (turrets have a minimum range)
-			                            if (rc.canAttackLocation(enemy.location)) {
-			                                rc.attackLocation(enemy.location);
-			                                break;
-			                            }
-			                        }
-			                    } else if (zombiesWithinRange.length > 0) {
-			                        for (RobotInfo zombie : zombiesWithinRange) {
-			                            if (rc.canAttackLocation(zombie.location)) {
-			                                rc.attackLocation(zombie.location);
-			                                break;
-			                            }
-			                        }
-			                    }
-	            			}
-	            		}
-	                }
-	            }
+                else 
+	            {
+	           		if(primer) 
+	           		{
+	           			++pack;
+	           			System.out.println(pack + "=======================PACKING=========================");
+	           			TTM = true;
+	           			rc.pack();
+	           		}
+	           		else 
+	           		{
+	           			MapLocation objective = readSignals();
+	           			if(objective != null)
+	           			{
+	           				if(rc.isWeaponReady()) rc.attackLocation(objective);
+	           			}
+	           			else
+	           			{
+		           			if (rc.isWeaponReady()) {
+		                    RobotInfo[] enemiesWithinRange = rc.senseNearbyRobots(attackRange, enemyTeam);
+		                    RobotInfo[] zombiesWithinRange = rc.senseNearbyRobots(attackRange, Team.ZOMBIE);
+		                    if (enemiesWithinRange.length > 0) {
+		                        for (RobotInfo enemy : enemiesWithinRange) {
+		                            // Check whether the enemy is in a valid attack range (turrets have a minimum range)
+		                            if (rc.canAttackLocation(enemy.location)) {
+		                                rc.attackLocation(enemy.location);
+		                                break;
+		                            }
+		                        }
+		                    } else if (zombiesWithinRange.length > 0) {
+		                        for (RobotInfo zombie : zombiesWithinRange) {
+		                            if (rc.canAttackLocation(zombie.location)) {
+		                                rc.attackLocation(zombie.location);
+		                                break;
+		                            }
+		                        }
+		                    }
+	           			}
+	           		}
+	               }
+	           }
                 Clock.yield();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
